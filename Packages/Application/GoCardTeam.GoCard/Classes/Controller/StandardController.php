e6ded1689442d253bd688fb28921cdfeb5a87566
Joshua Westerheide <dev@jdoubleu.de>
2017-05-10 19:32:01 +0200

    Kickstart application package
    Use flow cli to generate package code

---
+++ Packages/Application/GoCardTeam.GoCard/Classes/Controller/StandardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace GoCardTeam\GoCard\Controller;
+
+/*
+ * This file is part of the GoCardTeam.GoCard package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class StandardController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
