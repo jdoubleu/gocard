ef54cb75bd79ffb50d9e4a8d2a99f776d006304d
Max van Aerssen <max@vanaerssen.de>
2017-06-15 13:33:21 +0200

    Add reactstrap

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <BrowserRouter>
