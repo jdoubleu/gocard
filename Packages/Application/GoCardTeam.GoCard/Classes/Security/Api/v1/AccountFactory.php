d1cb3f94ddeeca5bb94108f77d18f4d384588ea9
Joshua Westerheide <dev@jdoubleu.de>
2017-06-17 16:23:24 +0200

    Extend default account repository and factory
    Add method to find access token by account referenced to account repository and provide method for generating access token in account factory.

---
+++ Packages/Application/GoCardTeam.GoCard/Classes/Security/Api/v1/AccountFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace GoCardTeam\GoCard\Security\Api\v1;
+
+use Neos\Flow\Security\Account;
+use \Neos\Flow\Security\AccountFactory as DefaultAccountFactory;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Class AccountFactory
+ * Extends default account factory to create an AccessToken account
+ *
+ * @package GoCardTeam\GoCard\Security\Api\v1
+ *
+ * @Flow\Scope("singleton")
+ */
+class AccountFactory extends DefaultAccountFactory
+{
+
+    /**
+     * Provider used for AccessTokens
+     */
+    const AccessTokenProviderName = 'AccessTokenProvider';
+
+    /**
+     * Default timeout TimeInterval for AccessTokens
+     */
+    const AccessTokenExpiration = "P1H";
+
+    /**
+     * @var AccountRepository
+     * @Inject
+     */
+    protected $accountRepository;
+
+    /**
+     * Creates an access_token for the given account
+     * @param Account $account
+     * @return Account
+     */
+    public function createAccessTokenForAccount(Account $account) : Account
+    {
+        $accessToken = $this->accountRepository->findActiveAccessTokenByAccountReferenceAndAuthenticationProviderName($account, self::AccessTokenProviderName);
+
+        if($accessToken === null) {
+            // Create new access token
+            $accessToken = new Account();
+            $accessToken->setAccountIdentifier(uniqid('', true));
+            $accessToken->setCredentialsSource($account->getAccountIdentifier());
+            $accessToken->setAuthenticationProviderName(self::AccessTokenProviderName);
+            $accessToken->setExpirationDate((new \DateTime())->add(new \DateInterval(self::AccessTokenExpiration)));
+        }
+
+        return $accessToken;
+    }
+}
---
+++ Packages/Application/GoCardTeam.GoCard/Classes/Security/Api/v1/AccountRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GoCardTeam\GoCard\Security\Api\v1;
+
+use Neos\Flow\Security\Account;
+use \Neos\Flow\Security\AccountRepository as DefaultAccountRepository;
+
+/**
+ * Class AccountRepository
+ * Extends the default AccountRepository by methods for AccessToken
+ *
+ * @package GoCardTeam\GoCard\Security\Api\v1
+ *
+ * @Flow\Scope("singleton")
+ */
+class AccountRepository extends DefaultAccountRepository
+{
+
+    /**
+     * Returns the AccessToken account which references the given account if it's not expired
+     *
+     * @param Account $account
+     * @param string $authenticationProviderName The authentication provider name
+     * @return Account|null AccessToken account or null if it's expired or doesn't exist
+     */
+    public function findActiveAccessTokenByAccountReferenceAndAuthenticationProviderName($account, $authenticationProviderName)
+    {
+        $query = $this->createQuery();
+        return $query->matching(
+            $query->logicalAnd(
+                $query->equals('credentialsSource', $account->getCredentialsSource()),
+                $query->equals('authenticationProviderName', $authenticationProviderName),
+                $query->logicalOr(
+                    $query->equals('expirationDate', null),
+                    $query->greaterThan('expirationDate', new \DateTime())
+                )
+            )
+        )->execute()->getFirst();
+    }
+}
