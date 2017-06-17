7cbdb3903136bd4ce639444aed81fad9814caaab
Timo R <timo@ribbers.me>
2017-06-17 17:43:31 +0200

    Add bootstrap to login scene

--- client/src/scenes/account/Login.js
+++ client/src/scenes/account/Login.js
@@ -1,32 +1,41 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {Form, FormGroup, Input, Card, CardGroup, CardTitle, CardText, Button} from 'reactstrap';
 
-class Login extends React.Component{
+class Login extends React.Component {
     render() {
         return (
             <div>
-                <div class="HSDLogin">
-                  <h2>HSD-Account</h2>
-                  <p>Studierende der Hochschule Düsseldorf haben die Möglichkeit, sich mit ihrem
-                  Hochschule Account anzumelden.</p>
-                  <a href="http://passport.hs-duesseldorf.de/default.aspx">Passwort vergessen?</a>
-                  <form>
-                    <button type="submit" name="HSDLogin">Anmelden mit HSD-Account</button>
-                    <Button color="danger">Anmelden mit HSD-Account</Button>
-                  </form>
-                </div>
+                <h1>Login</h1>
+                <p>Willkommen bei GoCard!</p>
+                <CardGroup>
+                        <Card block>
+                            <CardTitle>HSD-Account</CardTitle>
+                            <CardText>
+                                Studierende der Hochschule Düsseldorf haben die Möglichkeit, sich mit ihrem Hochschule Account anzumelden.<br/>
+                                <Link to="http://passport.hs-duesseldorf.de/default.aspx">Passwort vergessen?</Link>
+                            </CardText>
 
-                <div class="GoCardLogin">
-                  <h2>GoCard Account</h2>
-                  <p>Möchtest du einen GoCard Account erstellen?</p>
-                  <Link to="/register">GoCard Account erstellen</Link>
-                  <h3>Mit GoCard Account anmelden</h3>
-                  <form>
-                    <input type="email" name="loginEmail"></input>
-                    <input type="password" name="loginPassword"></input>
-                  </form>
-                </div>
+                            <Button outline color="primary">Anmelden mit HSD-Account</Button>
+                        </Card>
+                        <Card block>
+                            <CardTitle>GoCard Account</CardTitle>
+                            <CardText>
+                                Möchtest du einen GoCard Account erstellen? <br/>
+                                <Link to="/register">GoCard Account erstellen</Link>
+                            </CardText>
+
+                            <Form>
+                                <FormGroup>
+                                    <Input type="email" name="email" id="email" placeholder="E-Mail Adresse" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Input type="password" name="password" id="password" placeholder="Passwort" />
+                                </FormGroup>
+                                <Button outline block color="primary">Anmelden mit GoCard-Account</Button>
+                            </Form>
+                        </Card>
+                </CardGroup>
             </div>
         );
     }
