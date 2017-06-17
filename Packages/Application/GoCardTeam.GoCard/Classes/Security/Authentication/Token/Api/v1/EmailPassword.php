d26a81c2dbe539600e83f86fed3845898f260acd
Joshua Westerheide <dev@jdoubleu.de>
2017-06-17 14:46:10 +0200

    Add custom authentication token and its provider
    Also use this provider for authentication

---
+++ Packages/Application/GoCardTeam.GoCard/Classes/Security/Authentication/Token/Api/v1/EmailPassword.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GoCardTeam\GoCard\Security\Authentication\Token\Api\v1;
+
+use Neos\Flow\Mvc\ActionRequest;
+use Neos\Flow\Security\Authentication\Token\UsernamePassword;
+use Neos\Utility\ObjectAccess;
+
+/**
+ * Class EmailPassword
+ * Overwrites the default UsernamePassword token to work correctly with the following aspects:
+ * 1. A local GoCard account has an email instead of a username.
+ * 2. The API also defines that the email and password needs to be provided directly through the query params.
+ *
+ * @package GoCardTeam\GoCard\Security\Authentication\Token\Api\v1
+ */
+class EmailPassword extends UsernamePassword
+{
+
+    public function updateCredentials(ActionRequest $actionRequest)
+    {
+        $httpRequest = $actionRequest->getHttpRequest();
+        if ($httpRequest->getMethod() !== 'POST') {
+            return;
+        }
+
+        $arguments = $actionRequest->getInternalArguments();
+        $email = ObjectAccess::getPropertyPath($arguments, 'email');
+        $password = ObjectAccess::getPropertyPath($arguments, 'password');
+
+        if (!empty($username) && !empty($password)) {
+            $this->credentials['username'] = $email;
+            $this->credentials['password'] = $password;
+            $this->setAuthenticationStatus(self::AUTHENTICATION_NEEDED);
+        }
+    }
+}
