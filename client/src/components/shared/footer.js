bf8a81a8de0f091e81eec03ab672823cb5ce1ad0
Timo R <timo@ribbers.me>
2017-06-17 22:20:09 +0200

    Add footer center alignment center

--- client/src/components/shared/footer.js
+++ client/src/components/shared/footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
 
     render(){
         return (
-            <div class="Footer">
+            <div className="text-center">
                 <Container>
                     <hr />
                     <Link to="/help">Hilfe</Link>
