005a3d464ce1d78483f0c4a2b1bd37934922793b
Joshua Westerheide <dev@jdoubleu.de>
2017-06-17 16:23:54 +0200

    Add authentication controller for local accounts

---
+++ Packages/Application/GoCardTeam.GoCard/Classes/Controller/Api/v1/LocalAccountAuthenticationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GoCardTeam\GoCard\Controller\Api\v1;
+
+use GoCardTeam\GoCard\Security\Api\v1\AccountFactory;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Response;
+use Neos\Flow\Mvc\ActionRequest;
+use Neos\Flow\Security\Account;
+use Neos\Flow\Security\Authentication\Controller\AbstractAuthenticationController;
+use Neos\Flow\Security\Exception\AuthenticationRequiredException;
+
+class LocalAccountAuthenticationController extends AbstractAuthenticationController
+{
+
+    /**
+     * @var AccountFactory
+     * @Inject
+     */
+    protected $accountFactory;
+
+    /**
+     * Returns an access_token for api request with a 200 status
+     *
+     * @param ActionRequest $originalRequest
+     * @return string
+     */
+    protected function onAuthenticationSuccess(ActionRequest $originalRequest = null)
+    {
+        /** @var Account $account Logged in account */
+        $account = $this->authenticationManager->getSecurityContext()->getAccount();
+        $accessToken = $this->accountFactory->createAccessTokenForAccount($account);
+
+        /** @var Response $response */
+        $response = $this->controllerContext->getResponse();
+        $response->setStatus(200, 'Successfully logged in');
+        return $accessToken->getAccountIdentifier();
+    }
+
+    /**
+     * Send 400 error state according to API definition
+     *
+     * @param AuthenticationRequiredException|null $exception
+     */
+    protected function onAuthenticationFailure(AuthenticationRequiredException $exception = null)
+    {
+        /** @var Response $response */
+        $response = $this->controllerContext->getResponse();
+        $response->setStatus(400, 'Invalid email/password supplied');
+    }
+}
