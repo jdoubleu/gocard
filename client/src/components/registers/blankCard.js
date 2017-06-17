ccc318b720fbdccb624958a4991e7d915db479a3
Philipp <p.doll97@gmx.de>
2017-06-17 11:58:27 +0200

    Add link to

--- client/src/components/registers/blankCard.js
+++ client/src/components/registers/blankCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class BlankCard extends React.Component{
     render() {
@@ -9,11 +10,13 @@ class BlankCard extends React.Component{
                 <h2>Neue Karte Erstellen</h2>
                 <div>
                     <svg height={this.props.size} width={this.props.size}>
-                        <line x1="0" y1={this.props.size/2} x2={this.props.size} y2={this.props.size/2} strokeWidth="4" stroke="black" />
-                        <line x1={this.props.size/2} y1="0" x2={this.props.size/2} y2={this.props.size} strokeWidth="4" stroke="black" />
+                        <line x1="0" y1={this.props.size/2} x2={this.props.size} y2={this.props.size/2} strokeWidth={this.props.size/10} stroke="black" />
+                        <line x1={this.props.size/2} y1="0" x2={this.props.size/2} y2={this.props.size} strokeWidth={this.props.size/10} stroke="black" />
                     </svg>
                 </div>
+                <Link to="../scenes/register/Create">
                 <Button>Erstellen</Button>
+                    </Link>
             </div>
         );
     }
