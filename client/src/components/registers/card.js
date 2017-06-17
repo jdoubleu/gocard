c2c24403dac777ffc3ac8b84722cd8cc6c2b7a74
Max van Aerssen <max@vanaerssen.de>
2017-06-17 17:47:27 +0200

    Add Icon component to card

--- client/src/components/registers/card.js
+++ client/src/components/registers/card.js
@@ -4,12 +4,16 @@ import Icon from '../shared/user/icon'
 import { Button } from 'reactstrap';
 
 class Card extends React.Component{
+
     render() {
         return (
             <div>
               <h2>{this.props.title}</h2>
               <div>
-                {this.props.members.map((member) => {member})}
+                {this.props.members.map((member) => <Icon name={member} />)}
+              </div>
+              <div>
+                {/* Graph component */}
               </div>
               <Button>Öffnen</Button>
             </div>
