e4a27e88fbcb1a11cb143c1a4fad9e433af15b5f
Timo R <timo@ribbers.me>
2017-06-17 23:32:43 +0200

    Add padding to not found scene

--- client/src/scenes/errors/NotFound.js
+++ client/src/scenes/errors/NotFound.js
@@ -3,7 +3,7 @@ class NotFound extends React.Component {
 
     render() {
         return (
-            <div className="NotFound">
+            <div className="NotFound p-5 text-center">
                 <h2>404 - Page not found!</h2>
             </div>
         );
