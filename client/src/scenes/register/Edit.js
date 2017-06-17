2f1010e77eb93c3d2d16a994253ffaed1e63e497
Fabian Zipproth <fabian.zipproth@gmx.de>
2017-06-17 16:30:38 +0200

    Add register pages - create detail edit

--- client/src/scenes/register/Edit.js
+++ client/src/scenes/register/Edit.js
@@ -1,3 +1,56 @@
-/**
- * Created by Philipp on 11.06.2017.
- */
+import React, {Component} from 'react';
+import Icon from '../../components/shared/user/icon';
+//import Card from '../../components/registers/card';
+import { Button, InputGroup, Input, Container, Col, Row  } from 'reactstrap';
+class Create extends Component{
+
+    render() {
+        return (
+            <div>
+              <h2>Neues Register erstellen</h2>
+
+                <Container>
+                  <Row className="show-grid">
+                    <Col xs="8">
+                      <p>Titel des Registers </p>
+                      <InputGroup>
+                        <Input />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs="8">
+                      <p>Beschreibung </p>
+                      <InputGroup>
+                        <Input />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs="8">
+                      <p>Benutzer hinzufügen</p>
+                      <InputGroup>
+                        <Input />
+                      </InputGroup>
+                    </Col>
+
+                    <Col xs="8">
+                      <Icon name="Dimo Bibbers"/> NAME {/*this.props.user.name*/} ROLLE  {/*this.props.role.name*/} <Button>Edit</Button>
+                    </Col>
+
+                  </Row>
+
+                </Container>
+
+                <Button>Speichern</Button>
+                <Button>Abbrechen</Button>
+
+            </div>
+
+
+        );
+    }
+
+}
+
+export default Create;
