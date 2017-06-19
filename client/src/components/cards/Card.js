be661651b1aa6c7a247cbf0f44919b11f5f49296
Max van Aerssen <max@vanaerssen.de>
2017-06-19 11:15:56 +0200

    Add card for preview

--- client/src/components/cards/Card.js
+++ client/src/components/cards/Card.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {Button, Card as CardStrap, CardText, CardTitle, Col, Row} from "reactstrap";
+
 class Card extends React.Component {
     render() {
         return (
-            <div className="Card">
-                Oh eine karte!!
-            </div>
+            <Col xl="4" md="6" xs="12">
+                <CardStrap block className="mb-2">
+                    <CardTitle>{this.props.question}</CardTitle>
+                    <Button outline color="primary">Ansehen</Button>
+                </CardStrap>
+
+            </Col>
+
         );
     }
 }
+
+Card.propTypes = {
+    question: PropTypes.string.isRequired,
+};
+
 export default Card;
