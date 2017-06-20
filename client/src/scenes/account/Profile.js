cd8f3114273e7e3c7b81340bf19cd8e8c1acc0b4
Fabian Zipproth <fabian.zipproth@gmx.de>
2017-06-20 20:02:35 +0200

    Add layout of profile page

--- client/src/scenes/account/Profile.js
+++ client/src/scenes/account/Profile.js
@@ -1,9 +1,34 @@
 import React from "react";
+import {Button, Card, CardGroup, CardText, CardTitle, ButtonGroup} from "reactstrap";
+import { Link } from 'react-router-dom';
+import UserIcon from "../../components/shared/user/icon";
+import Statistic from "../../components/shared/statistic";
+
 class Profile extends React.Component {
+
+
+
     render() {
         return (
             <div>
-                Profile
+              <h1>Profile</h1>
+              <CardGroup>
+                <Card block>
+                  <CardTitle>Persönliche Einstellungen</CardTitle>
+                  {/* maybe zoom the icon */}
+                  <UserIcon name='Frank N Stein'/>
+                  {this.props.name}
+
+
+                  <CardText>
+
+                    <hr />
+
+                    <Link to="+">Bearbeiten</Link>
+                  </CardText>
+                  <CardText>Erfolge: <Statistic /></CardText>
+                </Card>
+              </CardGroup>
             </div>
         );
     }
