366af1b374176348c1ced316cd9ab2f080d400f2
Joshua Westerheide <dev@jdoubleu.de>
2017-06-21 21:03:06 +0200

    Add users endpoint controller and add default routing

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+/**
+ * Class UsersController
+ * Controller for serving /users endpoint
+ *
+ * @package GoCardTeam\GoCardApi\Controller\v1\Endpoint
+ */
+class UsersController extends ActionController
+{
+
+    /**
+     * Default Action
+     * Routed through default endpoint
+     */
+    public function defaultAction()
+    {
+    }
+}
