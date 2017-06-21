376a844fc291187fbace8007c3cb3b6dc63f3553
Max van Aerssen <max@vanaerssen.de>
2017-06-21 12:38:36 +0200

    Implement profile page

--- client/src/scenes/account/Profile.js
+++ client/src/scenes/account/Profile.js
@@ -1,6 +1,5 @@
 import React from "react";
-import {Card, CardGroup, CardText, CardTitle} from "reactstrap";
-import { Link } from 'react-router-dom';
+import {Card, Form, FormGroup, Input, Button, Label, Row, Col} from "reactstrap";
 import UserIcon from "../../components/shared/user/icon";
 
 class Profile extends React.Component {
@@ -8,21 +7,38 @@ class Profile extends React.Component {
       render() {
         return (
             <div>
-              <h1>Profile</h1>
-              <CardGroup>
+              <h1>Profil Einstellungen</h1>
                 <Card block>
-                  <CardTitle>Persönliche Einstellungen</CardTitle>
-                  {/* maybe zoom the icon */}
-                  <UserIcon name='Frank N Stein'/>
-                  {this.props.name}
-                  <CardText>
-
-                    <hr />
-
-                    <Link to="+">Bearbeiten</Link>
-                  </CardText>
-                </Card>
-              </CardGroup>
+                  <div className="mx-auto">
+                    <UserIcon name={this.props.name} diameter={200}/>
+                  </div>
+                  <Form onSubmit={this.handleSubmit}>
+                      <FormGroup>
+                          <Label for="name">Anzeigename</Label>
+                          <Input type="name" name="name" id="name" placeholder="Anzeigename"  required/>
+                      </FormGroup>
+                      <FormGroup>
+                          <Label for="email">E-Mail Adresse</Label>
+                          <Input type="email" name="email" id="email" placeholder="E-Mail Adresse" required/>
+                      </FormGroup>
+                      <FormGroup>
+                          <Label for="password">Passwort</Label>
+                          <Input type="password" name="password" id="password" placeholder="Passwort" required/>
+                      </FormGroup>
+                      <FormGroup>
+                          <Label for="confirmPassword">Passwort wiederholen</Label>
+                          <Input type="password" name="confirmPassword" id="confirmPassword" placeholder="Passwort wiederholen" required/>
+                      </FormGroup>
+                      <Row>
+                        <Col>
+                          <Button outline block color="danger">Abbrechen</Button>
+                        </Col>
+                        <Col>
+                          <Button outline block color="primary">Speichern</Button>
+                        </Col>
+                      </Row>
+                  </Form>
+              </Card>
             </div>
         );
     }
