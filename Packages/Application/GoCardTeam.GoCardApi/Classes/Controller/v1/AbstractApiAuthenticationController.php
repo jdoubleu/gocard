23c3228b2f907545a02dd2a409eb01500e38a416
Joshua Westerheide <dev@jdoubleu.de>
2017-06-22 12:39:41 +0200

    Generalize authentication controllers
    Add abstract authentication controller which sets up everything for this api.

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/AbstractApiAuthenticationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Controller\Api\v1;
+
+use Neos\Flow\Mvc\View\JsonView;
+use Neos\Flow\Security\Authentication\Controller\AbstractAuthenticationController;
+
+/**
+ * Class AbstractApiController
+ * Abstract API controller which does some preparation work, sets up
+ * everything the api needs.
+ * This differs from AbstractApiController in that it is ment for authentication controllers.
+ *
+ * @package GoCardTeam\GoCard\Controller\Api\v1
+ */
+abstract class AbstractApiAuthenticationController extends AbstractAuthenticationController
+{
+
+    /**
+     * @var string
+     */
+    protected $defaultViewObjectName = JsonView::class;
+
+    /**
+     * Override default supported types
+     * This API consumes 'application/json'
+     *
+     * @var array
+     */
+    protected $supportedMediaTypes = ["application/json"];
+
+    /**
+     * Overwrite default error method to not return any output.
+     * The error is handled via status code in onAuthenticationFailure method
+     *
+     * @return string
+     */
+    protected function errorAction()
+    {
+        return '';
+    }
+}
