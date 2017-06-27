25a70084aa80fcd9c6f66ea84025b7ec90e4b410
Joshua Westerheide <dev@jdoubleu.de>
2017-06-27 21:00:38 +0200

    Add empty user repository

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Repository/v1/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Domain\Repository\v1;
+
+use GoCardTeam\GoCardApi\Domain\Model\v1\User;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class UserRepository extends Repository
+{
+}
