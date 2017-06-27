406e4f9822d3a0cce7904451ef112d5e47c90f2d
Joshua Westerheide <dev@jdoubleu.de>
2017-06-27 21:00:56 +0200

    Implement getUserByIdAction to users endpoint

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/UsersController.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
 
+use GoCardTeam\GoCardApi\Domain\Model\v1\User;
+use GoCardTeam\GoCardApi\Domain\Repository\v1\UserRepository;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Controller\ActionController;
 
@@ -13,4 +15,25 @@ use Neos\Flow\Mvc\Controller\ActionController;
  */
 class UsersController extends ActionController
 {
+
+    /**
+     * @Flow\Inject
+     * @var UserRepository
+     */
+    protected $userRepository;
+
+    /**
+     * @param int $userId
+     * @return User
+     */
+    public function getUserByIdAction(int $userId)
+    {
+        /** @var User $user */
+        $user = $this->userRepository->findOneByUid($userId);
+        if($user === null) {
+            $this->throwStatus('User not found');
+        }
+
+        return $user;
+    }
 }
