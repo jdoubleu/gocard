9a613c58b485d024a76361836d5966afe25d13ef
Joshua Westerheide <dev@jdoubleu.de>
2017-07-02 15:55:39 +0200

    Implement flat property mapping for objects in current route
    Add aspect around `mergeArguments()` function of `DispatchComponent` class, which is responsible for merging arguments from the body with those mapped by the router from the get parameters.

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Http/v1/FlatPropertyMappingAspect.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Http\v1;
+
+use Neos\Flow\Aop\JoinPointInterface;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Routing\Router;
+use Neos\Flow\Reflection\ReflectionService;
+
+/**
+ * Aspect for mapping flat properties given in request body to a routed object.
+ * 
+ * @Flow\Aspect
+ */
+class FlatPropertyMappingAspect
+{
+
+    /**
+     * @Flow\Inject
+     * @var Router
+     */
+    protected $router;
+
+    /**
+     * @Flow\Inject
+     * @var ReflectionService
+     */
+    protected $reflectionService;
+
+    /**
+     * @param JoinPointInterface $joinPoint
+     * @Flow\Around("method(protected Neos\Flow\Mvc\DispatchComponent->mergeArguments())")
+     * @return mixed
+     */
+    public function mapFlatProperties(JoinPointInterface $joinPoint)
+    {
+        $result = $joinPoint->getAdviceChain()->proceed($joinPoint);
+
+        $route = $this->router->getLastMatchedRoute();
+        $routePartsConfiguration = $route->getRoutePartsConfiguration();
+        [$partsConfiguration, $targetName] = [end($routePartsConfiguration), key($routePartsConfiguration)];
+
+        if(!array_key_exists('objectType', $partsConfiguration)) {
+            return $result;
+        }
+
+        $classSchema = $this->reflectionService->getClassSchema($partsConfiguration['objectType']);
+
+        $plainArguments = array_filter($result, function ($argumentKey) {
+            return $argumentKey[0] != '@';
+        }, ARRAY_FILTER_USE_KEY);
+
+        if (!array_key_exists($targetName, $result)) {
+            $plainArguments[$targetName] = [];
+        } elseif (!is_array($result[$targetName])) {
+            return $result;
+        }
+
+        foreach ($classSchema->getProperties() as $prop => $value) {
+            if(!array_key_exists($prop, $plainArguments) || array_key_exists($prop, $result[$targetName])) {
+                continue;
+            }
+
+            unset($result[$prop]);
+            $result[$targetName][$prop] = $plainArguments[$prop];
+        }
+
+        return $result;
+    }
+}
