b89b98c6ce146cc11ae47e32f7987f529aeed741
Joshua Westerheide <dev@jdoubleu.de>
2017-07-02 17:34:20 +0200

    Remove default action with default route
    If there could no route be found an extern exception handler will manage to set the correct response.

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/AbstractApiEndpointController.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/AbstractApiEndpointController.php
@@ -27,13 +27,4 @@ abstract class AbstractApiEndpointController extends ActionController
      * @var array
      */
     protected $supportedMediaTypes = ["application/json"];
-
-    /**
-     * Default action for this endpoint.
-     * Might be called because there is no routing configuration for this endpoint so far.
-     */
-    public function defaultAction()
-    {
-        $this->throwStatus(400);
-    }
 }
