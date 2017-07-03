884d6861f5b687ef6b775ed35843f360f956ae1d
Joshua Westerheide <dev@jdoubleu.de>
2017-07-03 19:28:46 +0200

    Implement unique array items validator

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Validation/Validator/UniqueArrayItemsValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Validation\Validator;
+
+use Neos\Flow\Validation\Validator\AbstractValidator;
+
+/**
+ * Validator to verify distinct items in a collection
+ * Yet this validator only checks for unique items which are unique after converting them to strings.
+ */
+class UniqueArrayItemsValidator extends AbstractValidator
+{
+
+    /**
+     * Check if $value is valid. If it is not valid, needs to add an error
+     * to Result.
+     *
+     * @param mixed $value
+     * @return void
+     */
+    protected function isValid($value)
+    {
+        if (count(array_unique($value)) != count($value)) {
+            $this->addError('There are non-unique items in the given collection!', 1499102898);
+        }
+    }
+}
