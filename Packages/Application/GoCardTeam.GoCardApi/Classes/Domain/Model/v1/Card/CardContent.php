b1a7bf0aade572f8ab17e44bf48166e07fecb62a
Joshua Westerheide <dev@jdoubleu.de>
2017-07-04 19:28:49 +0200

    Add card content interface and abstract card content

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/Card/CardContent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Domain\Model\v1\Card;
+
+/**
+ * CardContent which will be used by a card.
+ * Subtypes might be ValueObjects
+ */
+interface CardContent
+{
+
+    /**
+     * Represent the card content as an array
+     * @return array
+     */
+    public function toArray() : array;
+
+    /**
+     * Serialize this card content
+     * @return string
+     */
+    public function toJSONString() : string;
+}
