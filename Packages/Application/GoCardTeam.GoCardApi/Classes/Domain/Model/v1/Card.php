39f62a0b3a0ba83ffa808a848f6cf2e22e95c6e2
Joshua Westerheide <dev@jdoubleu.de>
2017-07-05 20:10:01 +0200

    Simplify tags property of card model
    Remove unneeded ORM annotations.

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/Card.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/Card.php
@@ -38,8 +38,6 @@ class Card
 
     /**
      * @Flow\Validate(type="UniqueArrayItems")
-     * @ORM\Column(type="simple_array")
-     * @ORM\OneToMany(cascade={"persist"}, fetch="EAGER", orphanRemoval=true)
      * @var string[]
      */
     protected $tags;
