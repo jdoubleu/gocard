ea0d7e4ce2e6ac196b2b0f35637006e3dd6ef7d4
Joshua Westerheide <dev@jdoubleu.de>
2017-07-09 16:10:59 +0200

    Add update members action

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/MembersController.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/MembersController.php
@@ -7,7 +7,9 @@ use GoCardTeam\GoCardApi\Domain\Model\v1\Member;
 use GoCardTeam\GoCardApi\Domain\Model\v1\Register;
 use GoCardTeam\GoCardApi\Domain\Repository\v1\MembersRepository;
 use GoCardTeam\GoCardApi\Domain\Repository\v1\RegisterRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Property\TypeConverter\PersistentObjectConverter;
 
 /**
  * Members endpoint
@@ -35,5 +37,29 @@ class MembersController extends AbstractApiEndpointController
         $members = $register->getMembers();
 
         $this->view->assign('value', $members->toArray());
+    }
+
+    /**
+     * Allows property modification for update action.
+     * By default it is not allowed to modify a persisted object.
+     */
+    public function initializeUpdateMembersOfRegisterAction()
+    {
+        $membersConfiguration = $this->arguments->getArgument('members')->getPropertyMappingConfiguration();
+        $membersConfiguration->allowAllProperties();
+        $singleMemberConfiguration = $membersConfiguration->forProperty('*');
+        $singleMemberConfiguration->allowAllProperties()->skipProperties('usersUid');
+        $singleMemberConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_MODIFICATION_ALLOWED, true);
+    }
+
+    /**
+     * @param Register $register
+     * @param ArrayCollection<Member> $members
+     */
+    public function updateMembersOfRegisterAction(Register $register, $members)
+    {
+        $register->setMembers($members);
+
+        $this->registerRepository->update($register);
     }
 }
