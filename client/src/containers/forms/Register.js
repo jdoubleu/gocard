09804fde3ca2d22b7f5bed1bd16726b6658c86f5
Timo R <timo@ribbers.me>
2017-07-14 17:13:16 +0200

    Add redux new register form

---
+++ client/src/containers/forms/Register.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {Field, reduxForm} from "redux-form";
+import {loginUser} from "../../actions/auth";
+import {connect} from "react-redux";
+import {Alert, Button, Form} from "reactstrap";
+import InputField from "./fields/input";
+import Icon from "../../components/shared/user/icon";
+
+const validate = values => {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Required'
+    }
+
+    return errors
+};
+
+const RegisterForm = props => {
+    const {error, handleSubmit, submitting} = props;
+    return (
+        <Form onSubmit={handleSubmit}>
+            {
+                error &&
+                <Alert color="danger">
+                    {error}
+                </Alert>
+            }
+
+            <Field
+                name="title"
+                type="text"
+                component={InputField}
+                label="Title"
+            />
+
+            <Field
+                name="description"
+                type="textarea"
+                component={InputField}
+                label="Beschreibung"
+            />
+
+            <Button outline block color="primary" type="submit" disabled={submitting}>
+                Speichern
+            </Button>
+        </Form>
+    )
+};
+
+export default reduxForm({
+    form: 'registerForm',
+    validate
+})(RegisterForm);
