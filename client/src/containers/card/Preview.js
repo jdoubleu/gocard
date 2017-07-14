9a6ec6161950ec3e63f0c89d4297c0b201dccd54
Timo R <timo@ribbers.me>
2017-07-14 16:36:43 +0200

    Add card preview to container

--- client/src/containers/card/Preview.js
+++ client/src/containers/card/Preview.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Button, Card, CardTitle, Col} from "reactstrap";
+import {connect} from "react-redux";
 
 const Preview = ({card}) => {
     return (
         <Col xl="4" md="6" xs="12">
             <Card block className="mb-2">
-                <CardTitle>{this.props.card.question}</CardTitle>
+                <CardTitle>{card.question}</CardTitle>
                 <Button outline color="primary">Ansehen</Button>
             </Card>
         </Col>
@@ -15,7 +16,13 @@ const Preview = ({card}) => {
 };
 
 Preview.propTypes = {
-    card: PropTypes.object.isRequired,
+    cardId: PropTypes.number.isRequired
 };
 
-export default Preview;
+function mapStateToProps(state, ownProps) {
+    return {
+        card: state.entities.cards.byId[ownProps.cardId] || {},
+    }
+}
+
+export default connect(mapStateToProps)(Preview);
