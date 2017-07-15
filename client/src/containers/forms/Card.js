337da5ba68933889f0a8557aa011d9603af60eb4
Timo R <timo@ribbers.me>
2017-07-15 11:56:24 +0200

    Fix card form

---
+++ client/src/containers/forms/Card.js
@@ -0,0 +1,134 @@
+import React from "react";
+import {Field, formValueSelector, reduxForm} from "redux-form";
+import {Alert, Button, Form} from "reactstrap";
+import InputField from "./fields/input";
+import SelectButton from "./fields/selectButton";
+import InputSingleChoice from "./fields/inputSingleChoice";
+import InputMultipleChoice from "./fields/inputMultipleChoice";
+import {connect} from "react-redux";
+
+const validate = values => {
+    const errors = {};
+
+    if (!values.question) {
+        errors.question = 'Required'
+    }
+
+    if (!values.type) {
+        errors.type = 'Required'
+    }
+
+    if (!values.tags) {
+        errors.tags = 'Required'
+    }
+
+    return errors
+};
+
+const CardForm = props => {
+    const {error, handleSubmit, submitting, submitLabel, type} = props;
+    return (
+        <Form onSubmit={handleSubmit}>
+            {
+                error &&
+                <Alert color="danger">
+                    {error}
+                </Alert>
+            }
+
+            <Field
+                name="question"
+                type="text"
+                component={InputField}
+                label="Frage"
+            />
+
+            <Field
+                name="type"
+                component={SelectButton}
+                label="Fragetyp"
+                options={[
+                    {
+                        name: "Single Choice",
+                        value: "single-choice"
+                    },
+                    {
+                        name: "Multiple Choice",
+                        value: "multiple-choice"
+                    },
+                    {
+                        name: "Selbstkontrolle",
+                        value: "self-validate"
+                    },
+                    {
+                        name: "Texteingabe",
+                        value: "text-input"
+                    }
+                ]}
+            />
+
+            {
+                type === 'single-choice' &&
+                <Field
+                    name="content"
+                    type="text"
+                    component={InputSingleChoice}
+                    label="Antwort"
+                />
+            }
+
+            {
+                type === 'multiple-choice' &&
+                <Field
+                    name="content"
+                    type="text"
+                    component={InputMultipleChoice}
+                    label="Antwort"
+                />
+            }
+
+            {
+                type === 'self-validate' &&
+                <Field
+                    name="content"
+                    type="textarea"
+                    component={InputField}
+                    label="Antwort"
+                />
+            }
+
+            {
+                type === 'text-input' &&
+                <Field
+                    name="content"
+                    type="text"
+                    component={InputField}
+                    label="Antwort"
+                />
+            }
+
+            <Field
+                name="tags"
+                type="text"
+                component={InputField}
+                label="Tags"
+            />
+
+            <Button outline block color="primary" type="submit" disabled={submitting}>
+                {submitLabel}
+            </Button>
+        </Form>
+    )
+};
+
+const selector = formValueSelector('cardForm');
+function mapStateToProps(state) {
+    return {
+        type: selector(state, 'type')
+    }
+}
+
+export default connect(mapStateToProps)(reduxForm({
+    form: 'cardForm',
+    validate
+})(CardForm));
