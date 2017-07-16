ac02362aef67b1d4411a7321b4ada92a4b158d14
Joshua Westerheide <dev@jdoubleu.de>
2017-07-16 13:15:41 +0200

    Define cards endpoint and its controller

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/CardsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
+
+use GoCardTeam\GoCardApi\Controller\v1\AbstractApiEndpointController;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Cards endpoint
+ */
+class CardsController extends AbstractApiEndpointController
+{
+}
