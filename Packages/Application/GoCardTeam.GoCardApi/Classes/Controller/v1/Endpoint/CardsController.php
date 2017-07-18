eb8fbbe93ce43932d32fea77c8d8ed0ce9b294f1
Joshua Westerheide <dev@jdoubleu.de>
2017-07-18 18:58:11 +0200

    Add addMultipleCardsToRegister action

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/CardsController.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/CardsController.php
@@ -2,6 +2,7 @@
 
 namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use GoCardTeam\GoCardApi\Controller\v1\AbstractApiEndpointController;
 use GoCardTeam\GoCardApi\Domain\Model\v1\Card;
 use GoCardTeam\GoCardApi\Domain\Model\v1\Register;
@@ -111,5 +112,43 @@ class CardsController extends AbstractApiEndpointController
         $cards = $this->cardRepository->findByRegister($register->getUid());
 
         $this->view->assign('value', $cards);
+    }
+
+    /**
+     * Allows property modification for update action.
+     * By default it is not allowed to modify a persisted object.
+     */
+    public function initializeAddCardsToRegisterAction()
+    {
+        $cardsConfiguration = $this->arguments->getArgument('cards')->getPropertyMappingConfiguration();
+        $cardsConfiguration->allowAllProperties();
+        $singleCardConfiguration = $cardsConfiguration->forProperty('*');
+        $singleCardConfiguration->allowAllProperties()->skipProperties('uid', 'id');
+        $singleCardConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true);
+        $singleCardConfiguration->forProperty('content')->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true);
+        $contentConfiguration = $singleCardConfiguration->forProperty('content');
+        $contentConfiguration->allowAllProperties();
+        $contentConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_IDENTITY_CREATION_ALLOWED, true);
+        $contentConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED, true);
+        $contentConfiguration->setMapping('correct', 'correctAnswer')->setMapping('corrects', 'correctAnswers');
+    }
+
+    /**
+     * @param Register $register
+     * @param ArrayCollection<Card> $cards
+     */
+    public function addCardsToRegisterAction(Register $register, $cards)
+    {
+        $actualCards = $register->getCards();
+
+        foreach ($cards as $card) {
+            /** @var Card $card */
+            $card->setRegister($register);
+            $this->persistenceManager->add($card);
+
+            $actualCards->add($card);
+        }
+
+        $this->registerRepository->update($register);
     }
 }
