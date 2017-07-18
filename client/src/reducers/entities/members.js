bd111c743352b3ebb97872bb3efd944f5e579626
Philipp <philipp.doll@study.hs-duesseldorf.de>
2017-07-18 20:16:54 +0200

    Implement member reducer and action

--- client/src/reducers/entities/members.js
+++ client/src/reducers/entities/members.js
@@ -1,4 +1,71 @@
 import {combineReducers} from "redux";
+import {
+    LOAD_MEMBERS_SUCCESS,
+    UPDATE_MEMBERS_SUCCESS,
+    ADD_MEMBER_SUCCESS,
+    UPDATE_MEMBER_SUCCESS,
+    DELETE_MEMBER_SUCCESS,
+    LOAD_MEMBER_SUCCESS,
+    LOAD_MEMBERSHIPS_SUCCESS
+} from "../../actions/member";
+import _ from "lodash";
+
+function addMemberEntries(state, action) {
+
+}
+
+function addMemberEntry(state, action) {
+
+}
+
+function updateMemberEntry() {
+
+}
+
+function updateMemberEntries() {
+
+}
+
+function deleteMemberEntry() {
+
+}
+
+function updateMemberEntry() {
+
+}
+
+function addMembershipEntry() {
+
+}
+
+function addMemberIds(state, action) {
+
+}
+
+function addMemberId(state, action) {
+
+}
+
+function updateMemberId() {
+
+}
+
+function updateMemberIds() {
+
+}
+
+function deleteMemberId() {
+
+}
+
+function updateMemberId() {
+
+}
+
+function addMembershipId() {
+
+}
+
 
 function membersById(state = {}, action) {
     switch (action.type) {
