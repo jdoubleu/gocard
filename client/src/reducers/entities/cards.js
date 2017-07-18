9beabb5b966aaea74f45416580e2cf2776dfdba7
Philipp <philipp.doll@study.hs-duesseldorf.de>
2017-07-18 19:21:32 +0200

    Refactor cards reduxer

--- client/src/reducers/entities/cards.js
+++ client/src/reducers/entities/cards.js
@@ -2,27 +2,27 @@ import {combineReducers} from "redux";
 import {DELETE_CARD_SUCCESS, LOAD_CARDS_SUCCESS, UPDATE_CARD_SUCCESS, UPDATE_CARDS_SUCCESS} from "../../actions/card";
 import {_} from "lodash";
 
-function addMultipleCardIds(state, action) {
+function addCardIds(state, action) {
     const {response} = action;
     return _.union(state, _.map(response, 'id'));
 }
 
-function addCard(state, action) {
+function addCardEntry(state, action) {
     const {response} = action;
     return _.merge(state, _.keyBy(response, 'id'));
 }
 
-function updateSingleCardId(state, action) {
+function updateCardId(state, action) {
     const {response} = action;
     return _.concat(_.omit(state, response.id), response.id);
 }
 
-function updateMultipleCardIds(state, action) {
+function updateCardIds(state, action) {
     const {response} = action;
     return _.union(_.concat(state, _.map(response, 'id')));
 }
 
-function deleteCard(state, action) {
+function deleteCardEntry(state, action) {
     return _.omit(state, action.cardId);
 }
 
@@ -36,9 +36,9 @@ function cardsById(state = {}, action) {
         case LOAD_CARDS_SUCCESS:
         case UPDATE_CARD_SUCCESS:
         case UPDATE_CARDS_SUCCESS:
-            return addCard(state, action);
+            return addCardEntry(state, action);
         case DELETE_CARD_SUCCESS:
-            return deleteCard(state, action);
+            return deleteCardEntry(state, action);
         default:
             return state;
     }
@@ -47,11 +47,11 @@ function cardsById(state = {}, action) {
 function allCards(state = [], action) {
     switch (action.type) {
         case LOAD_CARDS_SUCCESS:
-            return addMultipleCardIds(state, action);
+            return addCardIds(state, action);
         case UPDATE_CARD_SUCCESS:
-            return updateSingleCardId(state, action);
+            return updateCardId(state, action);
         case UPDATE_CARDS_SUCCESS:
-            return updateMultipleCardIds(state, action);
+            return updateCardIds(state, action);
         case DELETE_CARD_SUCCESS:
             return deleteCardId(state, action);
         default:
