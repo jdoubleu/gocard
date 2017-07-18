02dbaf4303a55d1a6bd703f854cd1b29be0eebd8
Fabian Zipproth <fabian.zipproth@gmx.de>
2017-07-18 21:42:06 +0200

    Implement members reducer

--- client/src/reducers/entities/members.js
+++ client/src/reducers/entities/members.js
@@ -1,74 +1,91 @@
 import {combineReducers} from "redux";
 import {
-    LOAD_MEMBERS_SUCCESS,
-    UPDATE_MEMBERS_SUCCESS,
     ADD_MEMBER_SUCCESS,
-    UPDATE_MEMBER_SUCCESS,
     DELETE_MEMBER_SUCCESS,
     LOAD_MEMBER_SUCCESS,
-    LOAD_MEMBERSHIPS_SUCCESS
+    LOAD_MEMBERS_SUCCESS,
+    LOAD_MEMBERSHIPS_SUCCESS,
+    UPDATE_MEMBER_SUCCESS,
+    UPDATE_MEMBERS_SUCCESS
 } from "../../actions/member";
+
 import _ from "lodash";
 
-function addMemberEntries(state, action) {
-
-}
-
 function addMemberEntry(state, action) {
-
+    const {response} = action;
+    return _.merge(state, _.keyBy(response, 'id'));
 }
 
-function updateMemberEntry() {
-
+function addMemberEntries(state, action) {
+    const {response} = action;
+    return _.merge(state, _.keyBy(response, 'id'));
 }
 
-function updateMemberEntries() {
-
+function updateMemberEntry(state, action) {
+    const {response} = action;
+    return _.merge(state, _.keyBy(response, 'id'));
 }
 
-function deleteMemberEntry() {
-
+function updateMemberEntries(state, action) {
+    const {response} = action;
+    return _.merge(state, _.keyBy(response, 'id'));
 }
 
-function updateMemberEntry() {
-
+function deleteMemberEntry(state, action) {
+    return _.omit(state, action.id);
 }
 
-function addMembershipEntry() {
 
+function addMembershipEntry(state, action) {
+    const {response} = action;
+    return _.merge(state, _.keyBy(response, 'id'));
 }
 
 function addMemberIds(state, action) {
-
+    const {response} = action;
+    return _.union(state, _.map(response, 'id'));
 }
 
 function addMemberId(state, action) {
-
+    const {response} = action;
+    return _.union(state, _.map(response, 'id'));
 }
 
-function updateMemberId() {
-
+function updateMemberId(state, action) {
+    const {response} = action;
+    return _.concat(_.omit(state, response.id), response.id);
 }
 
-function updateMemberIds() {
-
+function updateMemberIds(state, action) {
+    const {response} = action;
+    return _.union(_.concat(state, _.map(response, 'id')));
 }
 
-function deleteMemberId() {
-
+function deleteMemberId(state, action) {
+    return _.pull(state, action.id);
 }
 
-function updateMemberId() {
 
+function addMembershipId(state, action) {
+    const {response} = action;
+    return _.union(_.concat(state, _.map(response, 'id')));
 }
-
-function addMembershipId() {
-
-}
-
 
 function membersById(state = {}, action) {
     switch (action.type) {
+        case ADD_MEMBER_SUCCESS:
+        case LOAD_MEMBER_SUCCESS:
+            return addMemberEntry(state, action);
+        case LOAD_MEMBERS_SUCCESS:
+            return addMemberEntries(state, action);
+        case UPDATE_MEMBERS_SUCCESS:
+            return updateMemberEntries(state, action);
+        case UPDATE_MEMBER_SUCCESS:
+            return updateMemberEntry(state, action);
+        case DELETE_MEMBER_SUCCESS:
+            return deleteMemberEntry(state, action);
+        case LOAD_MEMBERSHIPS_SUCCESS:
+            return addMembershipEntry(state, action);
         default:
             return state;
     }
@@ -76,6 +93,19 @@ function membersById(state = {}, action) {
 
 function allMembers(state = [], action) {
     switch (action.type) {
+        case ADD_MEMBER_SUCCESS:
+        case LOAD_MEMBER_SUCCESS:
+            return addMemberId(state, action);
+        case LOAD_MEMBERS_SUCCESS:
+            return addMemberIds(state, action);
+        case UPDATE_MEMBERS_SUCCESS:
+            return updateMemberIds(state, action);
+        case UPDATE_MEMBER_SUCCESS:
+            return updateMemberId(state, action);
+        case DELETE_MEMBER_SUCCESS:
+            return deleteMemberId(state, action);
+        case LOAD_MEMBERSHIPS_SUCCESS:
+            return addMembershipId(state, action);
         default:
             return state;
     }
