4afef962666c2ffe15d04707aed7bb9f2455e5fe
Joshua Westerheide <dev@jdoubleu.de>
2017-07-22 12:23:04 +0200

    Initialize activity endpoint controller and routes

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/ActivitiesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
+
+use GoCardTeam\GoCardApi\Controller\v1\AbstractApiEndpointController;
+use GoCardTeam\GoCardApi\Domain\Repository\v1\ActivityRepository;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Controller for /activities endpoint
+ */
+class ActivitiesController extends AbstractApiEndpointController
+{
+
+    /**
+     * @Flow\Inject
+     * @var ActivityRepository
+     */
+    protected $activityRepository;
+}
