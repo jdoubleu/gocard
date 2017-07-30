3a5dee6448410a308f89724c9bf3d5206bd8c7d6
Joshua Westerheide <dev@jdoubleu.de>
2017-07-30 13:06:16 +0200

    Add get card statistic action

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/CardStatisticsController.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Controller/v1/Endpoint/CardStatisticsController.php
@@ -3,6 +3,7 @@
 namespace GoCardTeam\GoCardApi\Controller\v1\Endpoint;
 
 use GoCardTeam\GoCardApi\Controller\v1\AbstractApiEndpointController;
+use GoCardTeam\GoCardApi\Domain\Model\v1\CardStatistic;
 use GoCardTeam\GoCardApi\Domain\Repository\v1\CardStatisticRepository;
 use Neos\Flow\Annotations as Flow;
 
@@ -17,4 +18,12 @@ class CardStatisticsController extends AbstractApiEndpointController
      * @var CardStatisticRepository
      */
     protected $cardStatisticRepository;
+
+    /**
+     * @param CardStatistic $cardStatistic
+     */
+    public function getCardStatisticByIdAction(CardStatistic $cardStatistic)
+    {
+        $this->view->assign('value', $cardStatistic);
+    }
 }
