a9001be872e85151e50493b3c3a0f037d9b07f09
Joshua Westerheide <dev@jdoubleu.de>
2017-08-04 16:32:52 +0200

    Update user context
    The current user will not be bound to the context object anymore.
    
    Temporary remove CacheAwareInterface because the generated cache identifier caused problems with the security context.

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Context/v1/UserContext.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Context/v1/UserContext.php
@@ -4,7 +4,6 @@ namespace GoCardTeam\GoCardApi\Context\v1;
 
 use GoCardTeam\GoCardApi\Domain\Model\v1\User;
 use GoCardTeam\GoCardApi\Domain\Repository\v1\UserRepository;
-use Neos\Cache\CacheAwareInterface;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
 use Neos\Flow\Security\Context as SecurityContext;
@@ -12,9 +11,11 @@ use Neos\Flow\Security\Context as SecurityContext;
 /**
  * Context which holds information about the current user, e.g. identified by the access.
  *
+ * TODO: Implement CacheAwareInterface
+ *
  * @Flow\Scope("singleton")
  */
-class UserContext implements CacheAwareInterface
+class UserContext
 {
 
     /**
@@ -36,33 +37,13 @@ class UserContext implements CacheAwareInterface
     protected $persistenceManager;
 
     /**
-     * A reference to the current user
-     *
-     * @var User
-     */
-    protected $user;
-
-    /**
      * Lazy and singleton getter for the current user
      *
      * @return User
      */
     public function getUser()
     {
-        if ($this->user === null) {
-            $this->user = $this->userRepository->findOneByAccount($this->securityContext->getAccount());
-        }
-        return $this->user;
-    }
-
-    /**
-     * Returns a string which distinctly identifies this object and thus can be used as an identifier for cache entries
-     * related to this object.
-     *
-     * @return string
-     */
-    public function getCacheEntryIdentifier()
-    {
-        return $this->getUser() ? $this->persistenceManager->getIdentifierByObject($this->getUser()) : null;
+        $account = $this->securityContext->getAccount();
+        return $this->userRepository->findOneByAccount($account);
     }
 }
