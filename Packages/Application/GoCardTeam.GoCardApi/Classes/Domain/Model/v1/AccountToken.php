1e81944998ec99845d53e8fc5496e828f045e1e7
Joshua Westerheide <dev@jdoubleu.de>
2017-08-07 19:18:37 +0200

    Model account tokens

---
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/AccountToken.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace GoCardTeam\GoCardApi\Domain\Model\v1;
+
+use Neos\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A special token used by registration and password reset
+ *
+ * @Flow\Entity
+ */
+class AccountToken
+{
+
+    /**
+     * @Flow\Validate(type="NotEmpty")
+     * @ORM\ManyToOne()
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @Flow\Validate(type="NotEmpty")
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @Flow\Validate(type="NotEmpty")
+     * @Flow\Validate(type="RegularExpression", options={"regularExpression"="(registration|password_reset)"})
+     * @ORM\Column(type="string", columnDefinition="SET('registration', 'password_reset')")
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+
+}
