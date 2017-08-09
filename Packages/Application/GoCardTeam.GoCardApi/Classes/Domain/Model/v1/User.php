cfd1e5fe96e5bb376e137e7e40a85f69a00898c3
Joshua Westerheide <dev@jdoubleu.de>
2017-08-09 12:12:55 +0200

    Update user model to cascade delete account reference

--- Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/User.php
+++ Packages/Application/GoCardTeam.GoCardApi/Classes/Domain/Model/v1/User.php
@@ -40,7 +40,7 @@ class User implements \JsonSerializable
     protected $email;
 
     /**
-     * @ORM\OneToOne()
+     * @ORM\OneToOne(orphanRemoval=true)
      * @var Account
      */
     protected $account;
