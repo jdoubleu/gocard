3b6d1c064ffa2a3f6ece13988fa93bd36097984b
Joshua Westerheide <dev@jdoubleu.de>
2017-08-09 18:16:11 +0200

    Update error reporting for setup in php production container

--- .docker/Production/php/setup.php
+++ .docker/Production/php/setup.php
@@ -58,7 +58,7 @@ class Utility
         Utility::log(implode("\n", $buffer));
 
         if ($code != 0) {
-            Utility::err("Failed to execute `$ex`");
+            Utility::err("Failed to execute `$ex`. Output:\n\n" . implode("\n", $buffer));
             exit(1);
         }
     }
